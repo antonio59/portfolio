cf7f671b76b6ae34dbf0f88ad9d1b9443f0c5dd9
Antonio Smith <9450209+antonio59@users.noreply.github.com>
2024-10-10 19:54:14 +0100

    Update App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
+import Certifications from './components/Certifications';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Certifications />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
